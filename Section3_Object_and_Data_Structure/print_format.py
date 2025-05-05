cc8441e64269bdd67f96269e8fb8556f663f553a
kwatanabe <begingree@gmail.com>
2025-05-05 11:03:26 +0900

    test print_format

--- Section3_Object_and_Data_Structure/print_format.py
+++ Section3_Object_and_Data_Structure/print_format.py
@@ -14,3 +14,9 @@ print('Then num is {:5.3f}'.format(num))
 
 name = 'Jose Maria'
 print(f'Hello {name}')
+
+testname = 'Jon'
+score1 = 100
+score2 = 300
+
+print(f'last night {testname} scored {str(score1+score2)} in the game')
