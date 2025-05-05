3b607c51b7a0036d0ac5eef3037ca4507fc66691
kwatanabe <begingree@gmail.com>
2025-05-05 09:37:11 +0900

    First commit

---
+++ Section3_Object_and_Data_Structure/strings.py
@@ -0,0 +1,13 @@
+# print('Hello \tworld\n')
+
+# print(len("Hello world"))
+
+test = "abcdefghijklmnopqrstuvwxyz"
+
+# print(test[0])
+# print(test[1])
+
+
+# print(test[0:5])
+# print(test[0:4:2])
+print(test[::-1])
---
+++ Section3_Object_and_Data_Structure/variable.py
@@ -0,0 +1,10 @@
+a = 30
+
+tax_rate = 0.1
+my_Salary = 100
+
+print(type(tax_rate))
+print(type(my_Salary))
+
+
+# print(f"My tax is {tax_rate*my_Salary}")
