d334f65d8126a556c55ea7e0b5722d24dc63e29e
kwatanabe <begingree@gmail.com>
2025-05-05 10:59:28 +0900

    added print_format

---
+++ Section3_Object_and_Data_Structure/print_format.py
@@ -0,0 +1,16 @@
+print('Please insert here {}'.format('test'))
+
+print('Please insert {},{},{}'.format('1','2','3'))
+print('Please insert {1},{0},{2}'.format('1','2','3'))
+print('Please insert {0},{0},{0}'.format('1','2','3'))
+
+
+print('Please insert {f},{t},{p}'.format(f = 'fox', t = 'tab', p = 'print'))
+
+
+num = 100 / 77
+print(num)
+print('Then num is {:5.3f}'.format(num))
+
+name = 'Jose Maria'
+print(f'Hello {name}')
