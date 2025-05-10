433c40401d2dcc841461b59fedfa0fc1de192836
kwatanabe <begingree@gmail.com>
2025-05-10 20:06:29 +0900

    fnished basic comparison operator

---
+++ Section4_Comparison_Operators/basic.py
@@ -0,0 +1,11 @@
+print('3' == 3)
+print('Test' == "test")
+print('test' == "test")
+
+
+print('\n1<2<3:',1 < 2 < 3)
+print('\n1<2 and 2<3:', 1<2 and 2<3)
+print('\n100<2 or 2<3:', 100<2 or 2<3)
+
+print('\n',not(1==1),sep='')
+print('\n',not(400>500),sep='')
