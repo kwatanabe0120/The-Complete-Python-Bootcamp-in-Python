0cecdf78bdb08be5e2439189b3ea8cb5edd049a7
kwatanabe <begingree@gmail.com>
2025-06-03 09:08:42 +0900

    added inheritance and polymorphism

---
+++ Section8_Object_Oriented_Programming/inheritance.py
@@ -0,0 +1,23 @@
+# Inheritance
+
+class Animal():
+    def __init__(self):
+        print('Animal created!')
+    def who_am_i(self):
+        print('I am an animal')
+    def eat(self):
+        print('I am eating')
+
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print('Dog created!')
+    
+    # overwrite animal's method
+    def eat(self):
+        print('I am a dog and eating')
+
+my_dog= Dog()
+my_dog.eat()
+
+
---
+++ Section8_Object_Oriented_Programming/polymorphism.py
@@ -0,0 +1,39 @@
+# Polymorphism
+
+class Dog():
+    def __init__(self,name):
+        self.name = name
+    
+    def speak(self):
+        print(f'My name is {self.name}, WOOF!')
+
+class Cat():
+    def __init__(self,name):
+        self.name = name
+    
+    def speak(self):
+        print(f'My name is {self.name}, MEOW!')
+
+def pet_speak(pet):
+    pet.speak()
+
+my_dog = Dog('Pon')
+my_cat = Cat('Hana')
+
+for animal in [my_dog, my_cat]:
+    animal.speak()
+    
+pet_speak(my_dog)
+
+# -----------------------------------
+class Animal1:
+    def speak(self):
+        raise NotImplementedError("Implement in Other class")
+
+class Dog1(Animal1):
+    def speak(self):
+        print("Woof!")
+    pass
+
+my_dog = Dog1()
+my_dog.speak()
