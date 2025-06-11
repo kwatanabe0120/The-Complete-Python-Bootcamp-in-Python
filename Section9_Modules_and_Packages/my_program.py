2a097a4c9fd519f8d2bd4e11dbfbef59f545ada7
kwatanabe <begingree@gmail.com>
2025-06-11 09:15:56 +0900

    finished section9 modules and packages

--- Section9_Modules_and_Packages/my_program.py
+++ Section9_Modules_and_Packages/my_program.py
@@ -1,3 +1,12 @@
-from my_module import my_func
+# How to import single module 
+# from my_module import my_func
 
-my_func()
+# my_func()
+
+
+# How to import package
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
